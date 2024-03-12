323296b0bc8521ec6313742264640273dcc84c5f
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-12 15:53:07 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,6 @@ function App () {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             <TodoForm />
-            <TodoForm />
-            <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {todos.map((todo) => (
